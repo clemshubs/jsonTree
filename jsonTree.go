c167ed9756a785f7057b7df9b131fd664f383193
Hubin-Andrieu Clement <hubinac@cnes.fr>
2018-02-16 08:48:23 +0100

    Fix forking with sequences

--- jsonTree.go
+++ jsonTree.go
@@ -37,6 +37,133 @@ func parse(filename string) Operation {
 	return c
 }
 
+func printArray(array [][]string){
+	
+	for i := 0; i < len(array); i++ {
+		for j := 0; j < len(array[i]);j++{
+			fmt.Print(array[i][j])
+		}
+		fmt.Printf("\n")
+	}
+
+}
+
+// This function returns the string array with the new box inserted
+// 
+// INPUTS
+// level : vertical position of top left corner
+// step : horizontal position of top left corner
+// box : string array of the box
+// output : the complete array of strings
+//
+// OUPUTS
+// the array with the new box
+func drawBox(level int, step int, box [][]string, output [][]string)(int,int,[][]string){
+
+	// if a line doesn't exist, we add it
+	if len(output)<level+4 {
+		outputL := len(output)
+		for i:=0; i<level+4-outputL; i++ {
+			output = append(output,[]string{})
+		}
+	}
+
+	fmt.Print("len output ")
+	fmt.Print(len(output))
+	fmt.Print("\n")
+	fmt.Print("level ")
+	fmt.Print(level )
+	fmt.Print("\n")
+	fmt.Print("step ")
+	fmt.Print(step)
+	fmt.Print("\n")
+	
+	// expanding output array
+	for i:=0; i<len(box); i++{
+		if len(output[i+level])<step+len(box[i]){
+			lengthOutput := len(output[i+level])
+			for j:=0; j<(step+len(box[i])-lengthOutput+1); j++{
+
+				output[i+level] = append(output[i+level]," ")
+			}
+		}
+	}
+
+	fmt.Print("len output ")
+	fmt.Print(len(output[level]))
+	fmt.Print("\n")
+	fmt.Print("level ")
+	fmt.Print(level)
+	fmt.Print("\n")
+
+
+	// copying box in output
+	for i:=0; i<len(box);i++{
+		for j:= 0; j< len(box[i]);j++{
+			output[level + i][step + j] = box[i][j]
+		}
+	}
+
+
+	return level,step+len(box[0]),output
+}
+
+
+// This function draws a line to fill the gap in a fork.
+//
+// INPUTS
+// level : vertical strip (top left corner)
+// step : final length needed
+// output: array of characters of the box before drawing
+//
+// OUTPUTS
+// tree with the new box
+func addLine(level int, stepBefore int, stepFinal int, output [][]string) ([][]string){
+
+	// arbitrary length of the box
+	// TODO change with the actual size.
+	
+	length := stepFinal-stepBefore
+	
+	height := 4
+	box :=  make([][]string,height)
+	fmt.Printf("level=%d\n",level)	
+	fmt.Printf("stepBefore=%d\n",stepBefore)	
+	fmt.Printf("stepFinal=%d\n",stepFinal)	
+	fmt.Printf("\n")
+
+
+
+	if length != 0 {
+
+
+
+		// init empty box
+
+		for i:= 0; i<height; i++ {
+			// 8 = 5 for the arrow, 3 for the final O
+			box[i] = make([]string,length)
+			for j:=0; j<len(box[i]);j++{
+				box[i][j]=" "
+			}
+
+		}
+
+		for j:=0; j<len(box[1])-1;j++{
+			box[1][j]="-"
+		}
+
+		//box[1][len(box[1])-1]=">"
+
+		_,_,output = drawBox(level,stepBefore,box,output)
+	}
+
+
+	printArray(output)
+	return output
+}
+
+
 // This function draws a box for an operation from its top left corner location
 //
 // INPUTS
@@ -46,54 +173,73 @@ func parse(filename string) Operation {
 // output: array of characters of the box before drawing
 //
 // OUTPUTS
-// bottom right corner vertical strip
+// top left corner vertical strip
 // bottom right corner horizontal strip
 // tree with the new box
-func drawBox(level int, step int, operation Operation, output [][]string) (int, int, [][]string){
+func addBox(level int, step int, operation Operation, output [][]string) (int, int, [][]string){
 
 	// arbitrary length of the box
 	// TODO change with the actual size.
-
+	
 	length := 10
+	height := 4
+
 	fmt.Printf(operation.Label)	
 	fmt.Printf("\n")
 	fmt.Printf("level=%d\n",level)	
 	fmt.Printf("step=%d\n",step)	
 	fmt.Printf("\n")
 
-	for i:=step; i<step+length+3; i++{
-		output[level][i] = "-"
+	// TODO : change this fixed array with dynamic allocation (append,...)
+	box :=  make([][]string,height)
+	for i:= 0; i<height; i++ {
+		// 8 = 5 for the arrow, 3 for the final O
+		box[i] = make([]string,length+6)
+		for j:=0; j<len(box[i]);j++{
+			box[i][j]=" "
+		}
+
 	}
 
-	output[level+1][step+1]=operation.Type_op
-	output[level+2][step+1]=operation.Label
-
-	for i:= step+2; i< step+1+len(operation.Type_op); i++{
-		output[level+1][i]=""
+	// drawing the top
+	for i:=4; i<length+3+2; i++{
+		box[0][i] = "-"
 	}
-	for i:= step+2; i< step+1+len(operation.Label); i++{
-		output[level+2][i]=""
-	}
-	
-	output[level+1][step] = "|"
-	output[level+1][step+length+2] = "|"
-	output[level+1][step-1] = ">"
-	output[level+1][step-2] = "-"
-	output[level+1][step-3] = "-"
-	output[level+1][step-4] = "-"
-	output[level+1][step+length+3] = "o"
 
-
-	output[level+2][step] = "|"
-	output[level+2][step+length+2] = "|"
-	for i:=step; i<step+length+3; i++{
-		output[level+3][i]="-"
+	// drawing bottom
+       	for i:=4; i<length+3+2; i++{
+		box[3][i]="-"
 	}
 	
-	step = step + length + 7
+	// drawing left 
+	box[2][4] = "|"
+	box[1][4] = "|"
 
-	return level,step,output
+	// drawing right
+	box[1][4+length] = "|"
+	box[2][4+length] = "|"
 
+	// drawing 'in' arrow
+	box[1][4-1] = ">"
+	box[1][4-2] = "-"
+	box[1][4-3] = "-"
+	box[1][4-4] = "-"
+
+	// dranwing 'final' o (to maybe be overwriten by next arrow)
+	box[1][4+length+1] = "o"
+
+	// writing info
+	box[1][4+1]=operation.Type_op
+	box[2][4+1]=operation.Label
+
+	for i:= 4+2; i< 4+1+len(operation.Type_op); i++{
+		box[1][i]=""
+	}
+	for i:= 4+2; i< 4+1+len(operation.Label); i++{
+		box[2][i]=""
+	}
+	
+	return drawBox(level,step,box,output)
 }
 
 // Functional drawing of the graph
@@ -105,9 +251,12 @@ func drawBox(level int, step int, operation Operation, output [][]string) (int, 
 func drawGraph(level int, step int, operations []Operation, output [][]string) (int, int, [][]string) { 
 
 	for _,operation := range operations {
-	
+
+
+//		fmt.Print(operation.Label)
+		fmt.Print("\n")
 		if operation.Type_op == "operation" {
-			level,step,output = drawBox(level,step,operation,output)
+			level,step,output = addBox(level,step,operation,output)
 		}
 
 		if operation.Type_op == "sequence"  {	
@@ -120,37 +269,61 @@ func drawGraph(level int, step int, operations []Operation, output [][]string) (
 		if operation.Type_op == "fork" {
 			child := operation.Children [0]
 			
-			output[level][step-3]="F"	
+
 			
 			stepBefore := step
-			//levelBefore := level
+			levelBefore := level
+			maxStep := step
+			tmp_step :=0
+			
+			level,tmp_step,output = drawGraph(level,step,[]Operation{child},output)
 
-			level,_,output = drawGraph(level,step,[]Operation{child},output)
-
+			if tmp_step > maxStep {
+				maxStep=tmp_step
+			}
 
 			for _,child := range operation.Children[1:] {
-			 	 level=level+5
+			 	 level=level+4
 				 
-				 output[level-3][stepBefore-3] = "|"	
-				 output[level-2][stepBefore-3] = "|"	
-				 output[level-1][stepBefore-3] = "|"	
-				 output[level][stepBefore-3] = "|"	
-				 output[level+1][stepBefore-3] = "|"	
-				 output[level+1][stepBefore-2] = "-"	
-				 output[level+1][stepBefore-1] = ">"
-				 level,step,output = drawGraph(level,stepBefore,[]Operation{child},output)
+				 level,tmp_step,output = drawGraph(level,stepBefore,[]Operation{child},output)
+				 output[level-3][stepBefore] = "|"	
+				 output[level-2][stepBefore] = "|"	
+				 output[level-1][stepBefore] = "|"	
+				 output[level][stepBefore] = "|"	
+				 output[level+1][stepBefore] = "|"
+				if tmp_step > maxStep {
+					maxStep=tmp_step
+				}
 			}
-			fmt.Printf("LEVEL %d",level)
+		
+			fmt.Printf("LEVEL %d",levelBefore)
 			fmt.Printf("STEP BEFORE %d\n",stepBefore)
 			i := level
 
-			for output[i][stepBefore-3] != "F" {
-				output[i][stepBefore-3]="|"
-				output[i-1][stepBefore-3]="|"
-				output[i-2][stepBefore-3]="|"
-				output[i-3][stepBefore-3]="|"
-				i=i-5
+			// Opening bracket
+
+			output[levelBefore][stepBefore]="F"	
+			for output[i][stepBefore] != "F" {
+				output[i][stepBefore]="|"
+				output[i-1][stepBefore]="|"
+				output[i-2][stepBefore]="|"
+				output[i-3][stepBefore]="|"
+				i=i-4
 			}
+
+			// Filling lines
+			for i:=levelBefore; i<level; i=i+4{
+				addLine(i,len(output[i])-2,maxStep+1,output)
+			}
+
+			// Closing bracket
+			for i:=levelBefore+2;i<level+1;i++ {
+				output[i][maxStep-1]="^"
+			}
+			printArray(output)
+			level = levelBefore
+			step = maxStep
+
 		}
 
 		if operation.Type_op == "conditionnal" {
@@ -168,22 +341,7 @@ func main() {
 
 	
 	// TODO : change this fixed array with dynamic allocation (append,...)
-	output :=  make([][]string,50)
-	for i:= 0; i<50; i++ {
-		output[i] = make([]string,100)
-	}
-	for i := 0; i < len(output); i++ {
-		for j := 0; j < len(output[i]);j++{
-			output[i][j]=" "
-		}
-	}
-
-	// First arrow
-	output[1][0]="-"
-	output[1][1]="-"
-	output[1][2]="-"
-	output[1][3]="-"
-	output[1][4]=">"
+	output :=  make([][]string,4)
 
 	fmt.Printf("\n")
 	fmt.Printf("Loading file and parsing\n")
@@ -194,15 +352,10 @@ func main() {
 	jsonContent = parse("D:/Utilisateurs/hubinac/Documents/test.goo")
 
 	// Computation of the tree
-	_,_,output = drawGraph(0,5,[]Operation{jsonContent},output)
+	_,_,output = drawGraph(0,0,[]Operation{jsonContent},output)
 
 	// Drawing of the tree
-	for i := 0; i < len(output); i++ {
-		for j := 0; j < len(output[i]);j++{
-			fmt.Print(output[i][j])
-		}
-		fmt.Printf("\n")
-	}
+	printArray(output)
 
 	// Bye
 	fmt.Printf("End of program")
